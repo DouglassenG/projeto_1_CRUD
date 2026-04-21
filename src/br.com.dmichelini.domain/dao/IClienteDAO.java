48b92fe31b7a6f68fb1adb3f686409d3e0bd266c
Douglas <douglasmsrodrigues@hotmail.com>
2026-04-21 20:07:08 -0300

    first commit

---
+++ src/br.com.dmichelini.domain/dao/IClienteDAO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package dao;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author dougl
+ */
+public interface IClienteDAO {
+
+    public boolean cadastrar(Cliente cliente);
+
+    public void excluir(long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
